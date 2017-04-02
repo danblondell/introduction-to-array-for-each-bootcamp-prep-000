f3f0405faefbdc31a52da2127ab37b390ffd727b
Dan Blondell <danblondell@gmail.com>
2017-04-02 03:23:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function square(n) {
   console.log(n * n)
 }
 
-var evens = [0, 2, 4, 6, 8, 10]
-
 function doToEvens(callback) {
   evens.forEach(callback)
 }
 
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
 function changeCompletely(element, index, array) {
-  array[index] = (Math.random()) * 100.toString() + '!!!'
+  array[index] = (Math.random() * 100).toString() + '!!!'
 }
 
 var animals = ["dog", "fish", "cat"]
